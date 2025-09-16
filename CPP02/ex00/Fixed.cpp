b9efc0a99abe5925ca7de769a566fd30905d5f84
Mohamed El Amrani <moel-amr@c1r7p3.1337.ma>
2025-09-16 21:36:12 +0100

    cpp02 ex00 ex01 ex02 done

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -1,33 +1,31 @@
 #include "Fixed.hpp"
 
+Fixed::Fixed() : _fixed_point(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+Fixed::~Fixed()
+{
+	std::cout << "Deconstructor called" << std::endl;
+}
+Fixed::Fixed(const Fixed &fix)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = fix;
+}
+Fixed &Fixed::operator=(const Fixed &fix)
+{
+	std::cout << "Copy assignement called" << std::endl;
+	this->_fixed_point = fix.getRawBits();
+	return (*this);
+}
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fixed_point);
+}
 
-
-        Fixed::Fixed() : _fixed_point(0)
-        {
-            std::cout<<"Default constructor called"<<std::endl;
-        }
-        Fixed::~Fixed(){
-            std::cout<<"Deconstructor called"<<std::endl;
-        }
-        Fixed::Fixed(const Fixed &fix)
-        {
-            std::cout<<"Copy constructor called"<<std::endl;
-            *this = fix;
-        }
-        Fixed &Fixed::operator=(const Fixed &fix)
-        {
-            std::cout<<"Copy assignement called"<<std::endl;
-            this->_fixed_point = fix.getRawBits();
-            return *this;
-        }
-        int Fixed::getRawBits( void ) const
-        {
-            std::cout<<"getRawBits member function called"<<std::endl;
-            return this->_fixed_point;
-        }
-
-        void Fixed::setRawBits( int const raw )
-        {
-            this->_fixed_point = raw;
-        }
-        
+void Fixed::setRawBits(int const raw)
+{
+	this->_fixed_point = raw;
+}
--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -3,20 +3,21 @@
 
 class Fixed
 {
-    private :
+    private:
         int _fixed_point;
         static const int fractional_bits = 8;
-    public :
+
+    public:
         Fixed();
         Fixed(const int n);
         Fixed(const float n);
         ~Fixed();
         Fixed(const Fixed &fix);
         Fixed &operator=(const Fixed &fix);
-        int getRawBits( void ) const;
-        void setRawBits( int const raw );
-        float toFloat( void ) const;
-        int toInt( void ) const;
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+        float toFloat(void) const;
+        int toInt(void) const;
 };
 
-std::ostream &operator<<(std::ostream &o,const Fixed &f);
+std::ostream &operator<<(std::ostream &o, const Fixed &f);
