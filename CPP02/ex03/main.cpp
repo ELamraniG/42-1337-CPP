504d5f4d33ed73bcde43292f131b89557906659a
Mohamed El Amrani <moel-amr@c1r7p3.1337.ma>
2025-09-18 15:34:35 +0100

    fixed ex03 test

--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -20,18 +20,18 @@ int	main(void)
 	Point b(6, 0);
 	Point c(3, 6);
 
-	Point p1(3, 2);   // inside T
-	Point p2(6, 0);   // on point b F
-	Point p3(3, 0);   // on edge F
-	Point p4(8, 3);   // outside F
-	Point p5(1, 5);   // outside F
-	Point p6(2, 3);   // inside T
+	Point p1(3, 2);   // inside
+	Point p2(6, 0);   // on point b 
+	Point p3(3, 0);   // on edge 
+	Point p4(8, 3);   // outside 
+	Point p5(1, 5);   // outside 
+	Point p6(2, 3);   // inside 
 
-	std::cout << "p1: " << (bsp(a, b, c, p1) == true ? "true" : "false") << std::endl;
-	std::cout << "p2: " << (bsp(a, b, c, p2) == true ? "true" : "false") << std::endl;
-	std::cout << "p3: " << (bsp(a, b, c, p3) == true ? "true" : "false") << std::endl;
-	std::cout << "p4: " << (bsp(a, b, c, p4) == true ? "true" : "false") << std::endl;
-	std::cout << "p5: " << (bsp(a, b, c, p5) == true ? "true" : "false") << std::endl;
-	std::cout << "p6: " << (bsp(a, b, c, p6) == true ? "true" : "false") << std::endl;
+	std::cout << "p1: " << (bsp(a, b, c, p1) == true ? "IN" : "OUT") << std::endl;
+	std::cout << "p2: " << (bsp(a, b, c, p2) == true ? "IN" : "OUT") << std::endl;
+	std::cout << "p3: " << (bsp(a, b, c, p3) == true ? "IN" : "OUT") << std::endl;
+	std::cout << "p4: " << (bsp(a, b, c, p4) == true ? "IN" : "OUT") << std::endl;
+	std::cout << "p5: " << (bsp(a, b, c, p5) == true ? "IN" : "OUT") << std::endl;
+	std::cout << "p6: " << (bsp(a, b, c, p6) == true ? "IN" : "OUT") << std::endl;
 	return (0);
 }
