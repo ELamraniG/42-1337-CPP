884145ebe55c399736c1f4a5bd7bb26530412619
ELamraniG <moha.mati@hotmail.com>
2025-10-25 00:23:22 +0100

    finished cpp06 ex00

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -1,15 +1,14 @@
 #include "ScalarConverter.hpp"
 #include <cctype>
+#include <climits>
 #include <cstdlib>
 #include <iomanip>
-#include <climits>
-
 
 bool	check_char(std::string string)
 {
 	if (string.length() != 1 || std::isdigit(string[0]))
-		return false;
-	return true;
+		return (false);
+	return (true);
 }
 
 bool	check_int(std::string s)
@@ -20,23 +19,23 @@ bool	check_int(std::string s)
 	i = 0;
 	if (s[0] != '-' && !std::isdigit(s[0]))
 	{
-		return false;
+		return (false);
 	}
 	if (s[0] == '-')
 		i++;
 	if (i == s.length())
 	{
-		return false;
+		return (false);
 	}
 	while (i < s.length())
 	{
 		if (!std::isdigit(s[i]))
 		{
-			return false;
+			return (false);
 		}
 		i++;
 	}
-	return true;
+	return (true);
 }
 bool	check_flaot(std::string s)
 {
@@ -49,7 +48,7 @@ bool	check_flaot(std::string s)
 	for (int j = 0; j < 3; j++)
 	{
 		if (science_stuff[j] == s)
-			return true;
+			return (true);
 	}
 	i = 0;
 	doted = false;
@@ -60,22 +59,22 @@ bool	check_flaot(std::string s)
 		i++;
 	}
 	if (i == s.length())
-		return false;
+		return (false);
 	while (i < s.length() - 1)
 	{
 		if (s[i] == '.')
 		{
 			if (doted == true || !std::isdigit(s[i + 1]))
-				return false;
+				return (false);
 			doted = true;
 		}
 		else if (!std::isdigit(s[i]))
-			return false;
+			return (false);
 		i++;
 	}
 	if (s[i] != 'f' || s.length() <= len || !doted)
-		return false;
-	return true;
+		return (false);
+	return (true);
 }
 bool	check_double(std::string s)
 {
@@ -88,7 +87,7 @@ bool	check_double(std::string s)
 	for (int j = 0; j < 3; j++)
 	{
 		if (science_stuff[j] == s)
-			return true;
+			return (true);
 	}
 	i = 0;
 	doted = false;
@@ -99,29 +98,28 @@ bool	check_double(std::string s)
 		i++;
 	}
 	if (i == s.length())
-		return false;
+		return (false);
 	while (i < s.length())
 	{
 		if (s[i] == '.')
 		{
 			if (doted == true || !std::isdigit(s[i + 1]))
-				return false;
+				return (false);
 			doted = true;
 		}
 		else if (!std::isdigit(s[i]))
-			return false;
+			return (false);
 		i++;
 	}
 	if (s.length() <= len || !doted)
-		return false;
-	return true;
+		return (false);
+	return (true);
 }
-void print_all(double d);
+void	print_all(double d);
 void ScalarConverter::convert(std::string s)
 {
-	
-	
-	double d;
+	double	d;
+
 	if (check_char(s) == true)
 	{
 		d = static_cast<char>(s[0]);
@@ -138,27 +136,86 @@ void ScalarConverter::convert(std::string s)
 	{
 		d = std::strtof(s.c_str(), NULL);
 	}
+	else
+	{
+		std::cout<<"either a literal ABzc... or number 42/42.25f/52.25"<<std::endl;
+		return;
+	}
 	print_all(d);
+}
+
+void	print_char(double d);
+void	print_int(double d);
+void	print_float(double d);
+void	print_double(double d);
+
+void	print_all(double d)
+{
+	print_char(d);
+	print_int(d);
+	print_float(d);
+	print_double(d);
 
 }
 
-
-
-
-
-void print_all(double d)
+void	print_char(double d)
 {
-	char c = static_cast<char>(d);
+	char	c = static_cast<char>(d);
 	if (static_cast<int>(d) < 0 || static_cast<int>(d) > 255)
 	{
-		std::cout<<"char : impossible"<<std::endl;
+		std::cout << "char : impossible" << std::endl;
 	}
-	else if (c <= static_cast<char>(13) || c == static_cast<char>(130))
+	else if (!std::isprint(c))
 	{
-		std::cout<<"char : not displayble"<<std::endl;
+		std::cout << "char : not displayble" << std::endl;
 	}
-	if (static_cast<int>(d) < INT_MIN || static_cast<int>(d) > INT_MIN)
+	else
 	{
+		std::cout << "char : " << c << std::endl;
+	}
+}
+void	print_int(double d)
+{
+	int n = static_cast<int>(d);
+	if (d != d)
 		std::cout<<"int : impossible"<<std::endl;
+	else if (static_cast<int>(d) >= INT_MIN && static_cast<int>(d) <= INT_MAX)
+	{
+		std::cout << "int : " << n<<std::endl;
 	}
+	else
+	{
+		std::cout << "int : impossible" << std::endl;
+	}
+}
+void	print_float(double d)
+{
+	float f = static_cast<int>(d);
+	bool is_inf = (d == std::numeric_limits<float>::infinity() || d == -std::numeric_limits<float>::infinity());
+	if (d != d)
+		std::cout<<"float : nanf"<<std::endl;
+	else if (is_inf)
+	{
+		if (d > 0)
+			std::cout<<"float : +inff"<<std::endl;
+		else
+			std::cout<<"float : -inff"<<std::endl;
+	}
+	else
+		std::cout << "float : " << std::fixed << std::setprecision(1)<<f <<"f" <<std::endl;
+}
+void	print_double(double d)
+{
+	bool is_inf = (d == std::numeric_limits<double>::infinity() || d == -std::numeric_limits<double>::infinity());
+	if (d != d)
+		std::cout<<"double : nan"<<std::endl;
+	else if (is_inf)
+	{
+		if (d > 0)
+			std::cout<<"double : +inf"<<std::endl;
+		else
+			std::cout<<"double : -inf"<<std::endl;
+	}
+	else
+		std::cout << "double : " << std::fixed << std::setprecision(1)<< d << std::endl;
 }
