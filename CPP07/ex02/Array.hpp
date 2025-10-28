9e6709bd8533e99f86c36503a9d1303511fc4820
ELamraniG <moha.mati@hotmail.com>
2025-10-28 23:28:13 +0100

    finished cpp07

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -1,53 +1,50 @@
 #pragma once
 #include <iostream>
 
-template <typename T>
-class Array 
-{
-	public :
-	unsigned int size;
-	T* array;
-	Array()
-	{
-		size = 0;
-		array = NULL;
-	}
-	Array(unsigned int n)
-	{
-		size = n;
-		array = new T[n];
-	}
-	~Array()
-	{
-		if (array)
-			delete []array;
-	}
-	Array(Array &cpy)
-	{
-		size = 0;
-		array = NULL;
-		*this = cpy;
-	}
-	Array &operator=(Array &cpy)
-	{
-		if (array)
-			delete []array;
-		array = NULL;
-		if (cpy.size == 0)
-			return *this;
-		array = new T[size];
-		for(int i = 0; i < cpy.get_size();i++)
-			array[i] = cpy[i];
-		return *this;
-	}
-	unsigned int get_size()
-	{
-		return size;
-	}
-	T& operator[](int index)
-	{
-		if (index < 0 || index >= get_size())
-			throw std::out_of_range("out of bound");
-		return array[index];
-	}
+template <typename T> class Array {
+private:
+  unsigned int arr_size;
+  T *array;
+
+public:
+  Array() {
+    arr_size = 0;
+    array = NULL;
+  }
+  Array(unsigned int n) {
+    arr_size = n;
+    array = new T[n];
+  }
+  ~Array() {
+    if (array)
+      delete[] array;
+  }
+  Array(const Array &cpy) {
+    arr_size = 0;
+    array = NULL;
+    *this = cpy;
+  }
+  Array &operator=(const Array &cpy) {
+    if (this == &cpy)
+      return *this;
+    if (array)
+      delete[] array;
+    array = NULL;
+    if (cpy.arr_size == 0)
+    {
+      arr_size = 0;
+      return *this;
+    }
+    this->arr_size = cpy.size();
+    array = new T[arr_size];
+    for (unsigned int i = 0; i < cpy.size(); i++)
+      array[i] = cpy[i];
+    return *this;
+  }
+  unsigned int size() const { return arr_size; }
+  T &operator[](unsigned int index) {
+    if (index >= size())
+      throw std::out_of_range("index out of range");
+    return array[index];
+  }
 };
