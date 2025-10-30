4886dcaa05e7a61de4c3024dca4d8783b0f1caf0
ELamraniG <moha.mati@hotmail.com>
2025-10-30 19:32:25 +0100

    startingCPP08

---
+++ CPP08/ex00/easyfind.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <vector>
+template<typename T>
+void easyfind(T x, int y)
+{
+
+}
---
+++ CPP08/ex00/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    
+
+	vector<int>::iterator v;
+
+	v3.insert(v3.begin() +5,155);
+	v = v3.begin();
+	int i = 0;
+    while(v != v3.end()) 
+	{
+		if (v[0] == 155)
+			return 0;
+		std::cout<<v[i]<<std::endl;
+		i++;
+		v++;
+    }
+    
+    return 0;
+}
+
+
+
+
+{
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(11);
+	sp.addNumber(17);
+	sp.addNumber(9);
+}
