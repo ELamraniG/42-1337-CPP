4566b6eeb160a2fd3d91b5c2917e969db5f236e8
ELamraniG <moha.mati@hotmail.com>
2025-12-27 22:26:00 +0100

    some cpp09 tests

--- cpp.cpp
+++ cpp.cpp
@@ -1,102 +1,40 @@
 
 #include <iostream>
-    struct ListNode {
-      int val;
-    ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
+#include <stdio.h>
+#include <vector>
+using namespace	std;
+void solve(string strings[10], string digits, string s,int digit_pos,vector<string> &sol)
+{
+	if (digit_pos == digits.size())
+	{
+		sol.push_back(s);
+		return ;
+	}
+	for (int i = 0; i < strings[digits[digit_pos] - '0'].length(); i++)
+	{
+		s += strings[digits[digit_pos] - '0'][i];
+		solve(strings, digits, s,digit_pos + 1,sol);
+		s.pop_back();
+	}
+}
+int	main(int ac, char **av)
+{
+	string	strings[10];
 
-#include <iostream>
-class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int reminder = 0;
-        struct ListNode *lista  = new struct ListNode ();
-        struct ListNode *l3  = lista;
-		int res;
-        while(l1 != NULL)
-        {
-            if (l2 == NULL)
-            {
-                while (l1 != NULL)
-                {
-					if (l1->val + reminder >= 10)
-					{
-						res = l3->val = l1->val + reminder;
-						l3->val = res % 10;
-						reminder = res / 10;
-						
-					}
-					else
-					{	
-						l3->val = l1->val + reminder;
-						reminder = 0;
-					}
-					l1 = l1->next;
-					if (l1 != NULL || reminder != 0) {
-						l3->next = new struct ListNode ();
-						l3 = l3->next;
-						l3->next = NULL;
-					}
-                }
-            }
-			else if (l1->val +l2->val + reminder >= 10)
-			{
-				res = (l2->val + l1->val + reminder);
-				l3->val = res % 10;
-				reminder = res / 10;
-				l1 = l1->next;
-				l2 = l2->next;
-				if (l1 != NULL || l2 != NULL || reminder != 0) {
-					l3->next = new struct ListNode ();
-					l3 = l3->next;
-				}
-			}
-			else if (l1->val +l2->val + reminder < 10)
-			{
-				res = (l2->val + l1->val + reminder );
-				reminder = 0;
-				l3->val = res;
-				l1 = l1->next;
-				l2 = l2->next;
-				if (l1 != NULL || l2 != NULL || reminder != 0) {
-					l3->next = new struct ListNode ();
-					l3 = l3->next;
-				}
-			}
-        }
-		while(l2 != NULL)
-		{
-        printf("here\n");
-			if (l2->val + reminder >= 10)
-					{
-						res = l3->val = l2->val + reminder;
-						l3->val = res % 10;
-						reminder = res / 10;
-						
-					}
-					else
-					{	
-						l3->val = l2->val + reminder;
-						reminder = 0;
-					}
-					l2 = l2->next;
-					if (l2 != NULL || reminder != 0) {
-						l3->next = new struct ListNode ();
-						l3 = l3->next;
-					}
-		}
-		while (reminder != 0)
-		{
-			l3->val = reminder % 10;
-			reminder = reminder/ 10;
-			if (reminder != 0) {
-				l3->next = new struct ListNode ();
-				l3 = l3->next;
-			}
-		}
-        return lista;
-    }
-};
+	strings[0] = "";
+	strings[1] = "";
+	strings[2] = "abc";
+	strings[3] = "def";
+	strings[4] = "ghi";
+	strings[5] = "jkl";
+	strings[6] = "mno";
+	strings[7] = "pqrs";
+	strings[8] = "tuv";
+	strings[9] = "wxyz";
+	string s = "";
+	
+	vector<string> soll;
+	solve(strings, av[1], s,0,soll);
+	// for (int i = 0;i < soll.size();i++)
+	// 	cout<<soll[i]<<std::endl;
+}
